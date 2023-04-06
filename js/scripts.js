14493b6c5492fda45cae9497a505ec796936ae74
Stewart <stilondanga@gmail.com>
2023-04-06 15:41:02 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+
+const main = document.querySelector('main');
+const rangeInput = document.querySelector('input[type="range"]');
+const outputValue = document.querySelector('output[data-value]');
+
+const getRangeValue = () => {
+  const value = rangeInput.value;
+  outputValue.textContent = value;
+  outputValue.dataset.value = value;
+  document.body.style.setProperty('--value', value)
+}
+window.addEventListener('DOMContentLoaded', getRangeValue)
+rangeInput.addEventListener('input', getRangeValue);
